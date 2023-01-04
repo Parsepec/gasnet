4785842a282d9a093a0e4ff93e2113e0042cd32f
parsepec <parsepecq@gmail.com>
2023-01-04 21:51:28 +0100

    corrected

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="w-full py-3 text-center border-b border-gray-100 drop-shadow-md z-0">
+    <div class=" py-3 text-center border-b border-gray-100 drop-shadow-md z-0">
+        <div class="bg-blue-500"></div>
         <h1 class="font-[500] text-2xl">Austoma Filling Station (Plot 178, Ahmadu Bello Way, Aminu Kano Crescent)</h1>
     </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,10 @@
 <template>
   <TopNav />
   <SideBar />
-  <div class="ml-12 w-full mt-16"><Header /></div>
+  <!-- <div class="flex ml-12 w-11/12 mt-16 justify-center">
+    <div class="w-10/12 px-12"><Header /></div>
+  </div> -->
+  <div class="ml-12 mt-16"><Header /></div>
   <div class="flex ml-12 w-11/12 mt-16 justify-end">
     <div class="w-10/12 px-12"><slot /></div>
   </div>
