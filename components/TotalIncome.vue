a686c2455c0c8e18a23e79f7991e18f7102f6230
parsepec <parsepecq@gmail.com>
2023-01-04 17:36:25 +0100

    deployed

--- components/TotalIncome.vue
+++ components/TotalIncome.vue
@@ -1,17 +1,18 @@
 <template>
   <div
-    class="flex flex-row rounded-md border justify-center border-gray-200 w-full mx-auto py-10"
+    class="flex flex-row rounded-md border justify-between border-gray-200 w-full mx-auto py-6"
   >
-    <div class="flex flex-row justify-around">
-      <div><p class="font-[600] text-gray-700 text-xl">Total Revenue</p></div>
-      <div>
-        <h1 class="text-6xl font-[600] justify-self-center">
-          N3,311,906.00
-          <span class="uppercase text-lg text-gray-500"
-            >TOTAL INCOME <span class="text-green-500">+N1,538,233</span>
-          </span>
-        </h1>
-      </div>
+    <div>
+      <p class="font-[600] text-gray-700 text-xl ml-6 self-start">Total Revenue</p>
     </div>
+    <div class="py-6">
+      <h1 class="text-6xl font-[600] justify-self-center">
+        N3,311,906.00
+        <span class="uppercase text-lg text-gray-500"
+          >TOTAL INCOME <span class="text-green-500">+N1,538,233</span>
+        </span>
+      </h1>
+    </div>
+    <div class="flex flex-row justify-start"></div>
   </div>
 </template>
